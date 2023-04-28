825a5fe66014ff32bed2285a727ce139b7720b53
KirkWest <kirkwestsooby@gmail.com>
2023-04-28 13:53:15 +1000

    altered classes function and added a menu function

--- functions.py
+++ functions.py
@@ -1,3 +1,8 @@
+def create_menu():
+    print("1. To create new character enter 1")
+    print("2. To edit previous character enter 2")
+    print("3. To see list of created characters enter 3")
+
 # dice roll function, rolls a certain amount of dice to be specified then removes the
 # lowest number as per DnD rules (usually 4 die, so only count the top 3 scores count)
 def dice_roll(num_dice):
@@ -14,7 +19,17 @@ def char_name():
 
 # list of classes available
 def print_classes():
-    classes = ["Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard"]
-    print("Select a class from the options below:")
-    for i, c in enumerate(classes):
-        print(f"{i+1}. {c}")
+    print("1. Barbarian")
+    print("2. Bard")
+    print("3. Cleric")
+    print("4. Druid")
+    print("5. Fighter")
+    print("6. Monk")
+    print("7. Paladin")
+    print("8. Ranger")
+    print("9. Rogue")
+    print("10. Sorcerer")
+    print("11. Warlock")
+    print("12. Wizard")
+    choice = input("Which class would you like? press '1-12' to select")
+    return choice
