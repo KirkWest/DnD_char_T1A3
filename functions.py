2acb82093b695abc24dff15cfc12d6ce47bda09b
KirkWest <kirkwestsooby@gmail.com>
2023-04-28 15:03:42 +1000

    started the promts for user input

--- functions.py
+++ functions.py
@@ -1,42 +1,20 @@
 def create_menu():
-    print("1. To create new character enter 1")
-    print("2. To edit previous character enter 2")
-    print("3. To see list of created characters enter 3")
-
-# dice roll function, rolls a certain amount of dice to be specified then removes the
-# lowest number as per DnD rules (usually 4 die, so only count the top 3 scores count)
-def dice_roll(num_dice):
-    import random
-    rolls = []
-    for i in range(num_dice):
-        rolls.append(random.randint(1, 6))
-    rolls.remove(min(rolls))
+    print("1. Create new character enter 1")
+    print("2. Edit previous character enter 2")
+    print("3. See list of created characters enter 3")
+    choice = input("Type the number (1-3): ")
+    return " "
+    
 
 # character name input def
 def char_name():
     name = input("What name would you like to call your character?: ")
-    return name
-
-# list of classes available
-def print_classes():
-    print(". Barbarian")
-    print("2. Bard")
-    print("3. Cleric")
-    print("4. Druid")
-    print("5. Fighter")
-    print("6. Monk")
-    print("7. Paladin")
-    print("8. Ranger")
-    print("9. Rogue")
-    print("10. Sorcerer")
-    print("11. Warlock")
-    print("12. Wizard")
-    choice = input("Which class would you like? press '1-12' to select")
-    return choice
+    return "Great name \n"
+    
 
 # made the classes a dictionary to clean it up, this way I can let the user just input an integer value
 # to select the class but only returns the class name. Hopefully cleaner for file handling
-def print_classes():
+def print_class():
     classes = {"1": "Barbarian",
                "2": "Bard",
                "3": "Cleric",
@@ -50,10 +28,20 @@ def print_classes():
                "11": "Warlock",
                "12": "Wizard"
                }
+    print("Class list")
     for i, c in classes.items():
         print(f"{i}. {c}")
-    choice = input("Which class would you like? Type the number (1-12) of the class you want to select:")
+    choice = input("Which class would you like? Choose from the above (1-12): ")
     return classes.get(choice)
 
-choice = print_classes()
-print("you've chosen " + choice)
+# dice roll function, rolls a certain amount of dice to be specified then removes the
+# lowest number as per DnD rules (usually 4 die, so only count the top 3 scores count)
+def dice_roll(num_dice):
+    import random
+    rolls = []
+    for i in range(num_dice):
+        rolls.append(random.randint(1, 6))
+    rolls.remove(min(rolls))
+    return sum(rolls)
+
+
--- main.py
+++ main.py
@@ -1,5 +1,23 @@
-print("Build your adventurer")
+# imports my functions
+from functions import create_menu, dice_roll, char_name, print_class
 
 
+attribute_scores = {}
+attribute_names = ["strength", "dexterity", "constitution", "intelligence", "wisdom" "charisma"]
+print("Welcome to you Dnd character creator, would you like to")
+
+print(create_menu())
+
+print(char_name())
+
+choice = print_class()
+print("you've chosen the " + choice + " class")
+
+attribute_names = ["strength", "dexterity", "constitution", "intelligence", "wisdom", "charisma"]
+
+for a in attribute_names:
+    score = dice_roll(4)
+    attribute_scores[a] = score
+print(f"Welcome, {char_name} the {print_class}, below are your attributes:")
 
  
